c0ae4d029ab87ac4a555f165e99e9b545322f774
evaaldo <evaaldormj@gmail.com>
2022-12-22 17:40:52 -0300

    feat: início da lógica de validação

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,16 +1,38 @@
 import Botao from "../../Components/Botao"
 import { InputPersonalizado } from "../../Components/Input/style"
 import { Container, Form } from "./styles"
+import { useState } from "react"
+import { validarEmail, validarSenha } from "../../Utils/Validadores"
 
 const Login = () => {
 
+    const [loading, setLoading] = useState(false)
+    const [form, setForm] = useState([])
+
     const handleChange = (event) => {
         console.log(event.target.value)
+        setForm({...form, [event.target.name]: event.target.value})
+        console.log('form', form)
     }
 
-    const handleSubmit = async () => {
-        alert('login')
+    const handleSubmit = async (event) => {
+
+        event.preventDefault()
+
+        try {
+            setLoading(true)
+            alert('login')
+            setLoading(false)
+        } catch (err) {
+            alert('Algo deu errado!' + err)
+        }
     }
+
+    const validadorInput = () => {
+        return validarEmail(form.email) && validarSenha(form.password)
+    }
+
+    console.log('form está válido?', validadorInput)
 
     return(
         <Container>
